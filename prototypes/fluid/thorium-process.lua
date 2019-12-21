8ca9da9c6d8c3c2541b85d2696c9161a6f3381af
korneel vandamme <krampus.sack.never@gmail.com>
2019-12-21 17:49:28 +0100

    new fluid and new textures for fluids

---
+++ prototypes/fluid/thorium-process.lua
@@ -0,0 +1,287 @@
+data:extend({
+	{
+        type = "fluid",
+        name = "Ammonium-tetrafluoroberyllate",
+        subgroup = "thorium-cahin",
+	    default_temperature = 25,
+		heat_capacity = "1KJ",
+		--42, 168, 55
+		--50, 199, 94
+	    base_color = {r=0.2, g=0.67, b=0.21},
+	    flow_color = {r=0.3, g=0.78, b=0.37},
+	    max_temperature = 100,
+        icon = "__bot365-nuclear-fuel-reprocessing__/graphics/icons/thorium_process/Ammonium-tetrafluoroberyllate.png",
+        icon_size = 32,
+	    pressure_to_speed_ratio = 0.4,
+	    flow_to_energy_ratio = 0.59,
+	    order = ""
+	},
+	{
+        type = "fluid",
+        name = "lithium-fluoride",
+        subgroup = "thorium-chain",
+	    default_temperature = 25,
+		heat_capacity = "1KJ",
+		--52, 235, 210
+		--14, 235, 219
+	    base_color = {r=0.20, g=0.92, b=0.82},
+	    flow_color = {r=0.05, g=0.92, b=0.85},
+	    max_temperature = 100,
+        icon = "__bot365-nuclear-fuel-reprocessing__/graphics/icons/thorium_processlithium-fluoride.png",
+        icon_size = 32,
+	    pressure_to_speed_ratio = 0.4,
+	    flow_to_energy_ratio = 0.59,
+	    order = ""
+	},
+	{
+        type = "fluid",
+        name = "berrylium-flouride",
+        subgroup = "thorium-chain",
+	    default_temperature = 25,
+		heat_capacity = "1KJ",
+		--160, 214, 21
+		--202, 255, 66
+	    base_color = {r=0.62, g=0.84, b=0.8},
+	    flow_color = {r=0.8, g=1, b=0.26},
+	    max_temperature = 100,
+        icon = "__bot365-nuclear-fuel-reprocessing__/graphics/icons/thorium_process/berrylium-flouride.png",
+        icon_size = 32,
+	    pressure_to_speed_ratio = 0.4,
+	    flow_to_energy_ratio = 0.59,
+	    order = ""
+	},
+	{
+        type = "fluid",
+        name = "reactor-grade-salt",
+        subgroup = "thorium-chain",
+	    default_temperature = 25,
+		heat_capacity = "1KJ",
+	    base_color = {r=0.95, g=0.95, b=0.95},
+	    flow_color = {r=1, g=1, b=1},
+	    max_temperature = 100,
+        icon = "__bot365-nuclear-fuel-reprocessing__/graphics/icons/thorium_process/reactor-grade-salt.png.png",
+        icon_size = 32,
+	    pressure_to_speed_ratio = 0.4,
+	    flow_to_energy_ratio = 0.59,
+	    order = ""
+	},
+	{
+        type = "fluid",
+        name = "molten-bismuth",
+        subgroup = "thorium-chain",
+	    default_temperature = 25,
+		heat_capacity = "1KJ",
+	    base_color = {r=0.5, g=0.5, b=0.5},
+	    flow_color = {r=0.6, g=0.6, b=0.6},
+	    max_temperature = 100,
+        icon = "__bot365-nuclear-fuel-reprocessing__/graphics/icons/thorium_process/molten-bismuth.png.png",
+        icon_size = 32,
+	    pressure_to_speed_ratio = 0.4,
+	    flow_to_energy_ratio = 0.59,
+	    order = ""
+	},
+	{
+        type = "fluid",
+        name = "pre-blancked-salt",
+        subgroup = "thorium-chain",
+	    default_temperature = 25,
+		heat_capacity = "1KJ",
+		--34, 5, 5
+		--105, 35, 35
+	    base_color = {r=0.5, g=0.5, b=0.5},
+	    flow_color = {r=0.41, g=0.32, b=0.32},
+	    max_temperature = 100,
+        icon = "__bot365-nuclear-fuel-reprocessing__/graphics/icons/thorium_process/pre-blancked-salt.png.png",
+        icon_size = 32,
+	    pressure_to_speed_ratio = 0.4,
+	    flow_to_energy_ratio = 0.59,
+	    order = ""
+	},
+	{
+        type = "fluid",
+        name = "post-blancked-salt",
+        subgroup = "thorium-chain",
+	    default_temperature = 25,
+		heat_capacity = "1KJ",
+		--156, 138, 79
+		--173, 152, 80
+	    base_color = {r=0.61, g=0.54, b=0.31},
+	    flow_color = {r=0.68, g=0.6, b=0.31},
+	    max_temperature = 100,
+        icon = "__bot365-nuclear-fuel-reprocessing__/graphics/icons/thorium_process/post-blancked-salt.png.png",
+        icon_size = 32,
+	    pressure_to_speed_ratio = 0.4,
+	    flow_to_energy_ratio = 0.59,
+	    order = ""
+	},
+	{
+        type = "fluid",
+        name = "thorium-bismuth",
+        subgroup = "thorium-chain",
+	    default_temperature = 25,
+		heat_capacity = "1KJ",
+		--73, 89, 74
+		--82, 112, 84
+	    base_color = {r=0.27, g=0.35, b=0.29},
+	    flow_color = {r=0.32, g=0.44, b=0.31},
+	    max_temperature = 100,
+        icon = "__bot365-nuclear-fuel-reprocessing__/graphics/icons/thorium_process/thorium-bismuth.png",
+        icon_size = 32,
+	    pressure_to_speed_ratio = 0.4,
+	    flow_to_energy_ratio = 0.59,
+	    order = ""
+	},
+	{
+        type = "fluid",
+        name = "high-concentrate-Pa-solution",
+        subgroup = "thorium-chain",
+	    default_temperature = 25,
+		heat_capacity = "1KJ",
+		--207, 209, 144
+		--168, 100, 84
+	    base_color = {r=0.81, g=0.82, b=0.56},
+	    flow_color = {r=0.65, g=0.39, b=0.32},
+	    max_temperature = 100,
+        icon = "__bot365-nuclear-fuel-reprocessing__/graphics/icons/thorium_process/high-concentrate-Pa-solution.png",
+        icon_size = 32,
+	    pressure_to_speed_ratio = 0.4,
+	    flow_to_energy_ratio = 0.59,
+	    order = ""
+	},
+	{
+        type = "fluid",
+        name = "low-concentrate-Pa-solution",
+        subgroup = "thorium-chain",
+	    default_temperature = 25,
+		heat_capacity = "1KJ",
+		--172, 173, 144
+		--154, 100, 76
+	    base_color = {r=0.67, g=0.68, b=0.56},
+	    flow_color = {r=0.60, g=0.39, b=0.29},
+	    max_temperature = 100,
+        icon = "__bot365-nuclear-fuel-reprocessing__/graphics/icons/thorium_process/low-concentrate-Pa-solution.png",
+        icon_size = 32,
+	    pressure_to_speed_ratio = 0.4,
+	    flow_to_energy_ratio = 0.59,
+	    order = ""
+	},
+	{
+        type = "fluid",
+        name = "protactinium-salt",
+        subgroup = "thorium-chain",
+	    default_temperature = 25,
+		heat_capacity = "1KJ",
+		--232, 235, 176
+		--253, 255, 209
+	    base_color = {r=0.90, g=0.92, b=0.69},
+	    flow_color = {r=1, g=1, b=0.82},
+	    max_temperature = 100,
+        icon = "__bot365-nuclear-fuel-reprocessing__/graphics/icons/thorium_process/protactinium-salt.png",
+        icon_size = 32,
+	    pressure_to_speed_ratio = 0.4,
+	    flow_to_energy_ratio = 0.59,
+	    order = ""
+	},
+	{
+        type = "fluid",
+        name = "uranium-decay-salt",
+        subgroup = "thorium-chain",
+	    default_temperature = 25,
+		heat_capacity = "1KJ",
+		--145, 255, 171
+		--111, 237, 141
+	    base_color = {r=0.56, g=1, b=0.67},
+	    flow_color = {r=0.43, g=0.93, b=0.55},
+	    max_temperature = 100,
+        icon = "__bot365-nuclear-fuel-reprocessing__/graphics/icons/thorium_process/uranium-decay-salt.png",
+        icon_size = 32,
+	    pressure_to_speed_ratio = 0.4,
+	    flow_to_energy_ratio = 0.59,
+	    order = ""
+	},
+	{
+        type = "fluid",
+        name = "hydroge-hydrofluoride",
+        subgroup = "thorium-chain",
+	    default_temperature = 25,
+		heat_capacity = "1KJ",
+		--255, 255, 10
+		--237, 220, 28
+	    base_color = {r=1, g=1, b=0.05},
+	    flow_color = {r=93, g=0.86, b=0.1},
+	    max_temperature = 100,
+        icon = "__bot365-nuclear-fuel-reprocessing__/graphics/icons/thorium_process/hydrogen-hydrofluoride.png",
+        icon_size = 32,
+	    pressure_to_speed_ratio = 0.4,
+	    flow_to_energy_ratio = 0.59,
+	    order = ""
+	},
+	{
+        type = "fluid",
+        name = "fresh-reactor-salt",
+        subgroup = "thorium-chain",
+	    default_temperature = 25,
+		heat_capacity = "1KJ",
+		--43, 224, 27
+		--33, 181, 20
+	    base_color = {r=1, g=1, b=0.05},
+	    flow_color = {r=93, g=0.86, b=0.1},
+	    max_temperature = 100,
+        icon = "__bot365-nuclear-fuel-reprocessing__/graphics/icons/thorium_process/fresh-reactor-salt.png",
+        icon_size = 32,
+	    pressure_to_speed_ratio = 0.4,
+	    flow_to_energy_ratio = 0.59,
+	    order = ""
+	},
+	{
+        type = "fluid",
+        name = "waste-reactor-salt",
+        subgroup = "thorium-chain",
+	    default_temperature = 25,
+		heat_capacity = "1KJ",
+		--15, 87, 9
+		--197, 33, 228
+	    base_color = {r=1, g=1, b=0.05},
+	    flow_color = {r=93, g=0.86, b=0.1},
+	    max_temperature = 100,
+        icon = "__bot365-nuclear-fuel-reprocessing__/graphics/icons/thorium_process/waste-reactor-salt.png",
+        icon_size = 32,
+	    pressure_to_speed_ratio = 0.4,
+	    flow_to_energy_ratio = 0.59,
+	    order = ""
+	},
+	{
+        type = "fluid",
+        name = "waste-salt",
+        subgroup = "thorium-chain",
+	    default_temperature = 25,
+		heat_capacity = "1KJ",
+		--120, 14, 158
+		--84, 12, 110
+	    base_color = {r=1, g=1, b=0.05},
+	    flow_color = {r=93, g=0.86, b=0.1},
+	    max_temperature = 100,
+        icon = "__bot365-nuclear-fuel-reprocessing__/graphics/icons/thorium_process/waste-salt.png",
+        icon_size = 32,
+	    pressure_to_speed_ratio = 0.4,
+	    flow_to_energy_ratio = 0.59,
+	    order = ""
+	},
+	{
+        type = "fluid",
+        name = "bismuth-waste",
+        subgroup = "thorium-chain",
+	    default_temperature = 25,
+		heat_capacity = "1KJ",
+		--151, 125, 161
+		--156, 109, 173
+	    base_color = {r=1, g=1, b=0.05},
+	    flow_color = {r=93, g=0.86, b=0.1},
+	    max_temperature = 100,
+        icon = "__bot365-nuclear-fuel-reprocessing__/graphics/icons/thorium_process/waste-bismuth.png",
+        icon_size = 32,
+	    pressure_to_speed_ratio = 0.4,
+	    flow_to_energy_ratio = 0.59,
+	    order = ""
+	}
+})
